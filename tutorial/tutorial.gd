9c84ecefd89949ba7596f23497acdcb523c539e3
willyjinx96 <willy.fer@hotmail.com>
2020-04-03 04:28:39 -0400

    Botones Tutorial

--- tutorial/tutorial.gd
+++ tutorial/tutorial.gd
@@ -5,3 +5,19 @@ func _on_btn_jugar_pressed():
 
 func _on_btn_menu_pressed():
 	get_tree().change_scene("res://Inicio/Inicio.tscn")
+
+func _ready():
+	animation="tuto1"
+
+func _on_next_pressed():
+	$prev.disabled=false
+	animation="tuto2"
+	$next.disabled=true
+	pass # Replace with function body.
+
+
+func _on_prev_pressed():
+	$next.disabled=false
+	animation="backtuto1"
+	$prev.disabled=true
+	pass # Replace with function body.
